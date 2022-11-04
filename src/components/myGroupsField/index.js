f51cb2cf6fe89d06caef85482ac5323c97a4a63c
Nafis Abdullah <n.ahmad.web.cit22@gmail.com>
2022-11-05 00:06:49 +0600

    new

--- src/components/myGroupsField/index.js
+++ src/components/myGroupsField/index.js
@@ -121,7 +121,7 @@ const MyGroupsField = () => {
               {grpName}
             </p>
             <p className="font-semibold text-[17px] text-primary">
-              Group Requests
+              Group Request
             </p>
             {grpReqList.map(
               (item) =>
